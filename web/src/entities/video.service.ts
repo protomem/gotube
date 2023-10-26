848cd5ea24735f43532979d290a44801918a2c5d
protomem <protomem0709@gmail.com>
2023-10-26 13:08:15 +0300

    feat(web): get videos by user nickname

--- web/src/entities/video.service.ts
+++ web/src/entities/video.service.ts
@@ -1,1 +1,21 @@
-export const videoService = {};
+import { apiClient } from "@/entities/api";
+import { Video } from "@/entities/models";
+
+export interface GetVideosByUserNicknameRequest {
+  userNickname: string;
+}
+
+export interface GetVideosByUserNicknameResponse {
+  videos: Video[];
+}
+
+export const videoService = {
+  getVideosByUserNickname: async ({
+    userNickname,
+  }: GetVideosByUserNicknameRequest) => {
+    const response = await apiClient.get<GetVideosByUserNicknameResponse>(
+      `/videos/?user=${userNickname}`,
+    );
+    return response.data;
+  },
+};
--- web/src/pages/profile.tsx
+++ web/src/pages/profile.tsx
@@ -9,7 +9,7 @@ import { useNavigate, useParams } from "react-router-dom";
 import ProfilePane from "@/widgets/profile-pane";
 import { Box, Divider } from "@mui/joy";
 import VideoGrid from "@/feature/video-grid";
-import Paginator from "@/shared/paginator";
+import { videoService } from "@/entities/video.service";
 
 export function Profile() {
   const nav = useNavigate();
@@ -29,101 +29,18 @@ export function Profile() {
     enabled: userNickname !== undefined,
   });
 
-  const videos: Video[] = [
-    {
-      id: "1",
-      createdAt: new Date(),
-      updatedAt: new Date(),
-      title: "Video 1",
-      description: "Description 1",
-      thumbnailPath:
-        "https://images.unsplash.com/photo-1532614338840-ab30cf10ed36?auto=format&fit=crop&w=318",
-      videoPath: "https://example.com/video1.mp4",
-      isPublic: true,
-      views: 100,
-      user: {
-        id: "1",
-        createdAt: new Date(),
-        updatedAt: new Date(),
-        nickname: "user1",
-        email: "0pEeH@example.com",
-        isVerified: true,
-        avatarPath: "https://example.com/user1.jpg",
-        description: "Description 1",
-      },
+  const [videos, setVideos] = useState<Video[]>([]);
+  useQuery({
+    queryKey: ["videos", userNickname],
+    queryFn: async () =>
+      videoService.getVideosByUserNickname({
+        userNickname: user?.nickname || "",
+      }),
+    onSuccess: (data) => {
+      setVideos(data.videos);
     },
-    {
-      id: "2",
-      createdAt: new Date(),
-      updatedAt: new Date(),
-      title: "Video 2",
-      description: "Description 2",
-      thumbnailPath:
-        "https://images.unsplash.com/photo-1532614338840-ab30cf10ed36?auto=format&fit=crop&w=318",
-      videoPath: "https://example.com/video2.mp4",
-      isPublic: true,
-      views: 200,
-      user: {
-        id: "2",
-        createdAt: new Date(),
-        updatedAt: new Date(),
-        nickname: "user2",
-        email: "0pEeH@example.com",
-        isVerified: true,
-        avatarPath: "https://example.com/user2.jpg",
-        description: "Description 2",
-      },
-    },
-    {
-      id: "3",
-      createdAt: new Date(),
-      updatedAt: new Date(),
-      title: "Video 3",
-      description: "Description 3",
-      thumbnailPath:
-        "https://images.unsplash.com/photo-1532614338840-ab30cf10ed36?auto=format&fit=crop&w=318",
-      videoPath: "https://example.com/video3.mp4",
-      isPublic: true,
-      views: 300,
-      user: {
-        id: "3",
-        createdAt: new Date(),
-        updatedAt: new Date(),
-        nickname: "user3",
-        email: "0pEeH@example.com",
-        isVerified: true,
-        avatarPath: "https://example.com/user3.jpg",
-        description: "Description 3",
-      },
-    },
-    {
-      id: "4",
-      createdAt: new Date(),
-      updatedAt: new Date(),
-      title: "Video 4",
-      description: "Description 4",
-      thumbnailPath:
-        "https://images.unsplash.com/photo-1532614338840-ab30cf10ed36?auto=format&fit=crop&w=318",
-      videoPath: "https://example.com/video4.mp4",
-      isPublic: true,
-      views: 400,
-      user: {
-        id: "4",
-        createdAt: new Date(),
-        updatedAt: new Date(),
-        nickname: "user4_ddddd_daaaaaaaaaaaa",
-        email: "0pEeH@example.com",
-        isVerified: true,
-        avatarPath: "https://example.com/user4.jpg",
-        description: "Description 4",
-      },
-    },
-  ];
-  videos.push(...videos);
-  videos.push(...videos);
-  videos.push(...videos);
-
-  const [page, setPage] = useState(1);
+    enabled: user !== null,
+  });
 
   return (
     <Layout>
@@ -165,10 +82,6 @@ export function Profile() {
             }}
           >
             <VideoGrid videos={videos} />
-          </Box>
-
-          <Box style={{ flex: 1, alignSelf: "center", marginTop: 10 }}>
-            <Paginator page={page} onChangePage={setPage} maxPage={10} />
           </Box>
         </Box>
       )}
