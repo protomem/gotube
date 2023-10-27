31ace6879d49cd4116b064d90b694bf7b9a63f83
protomem <protomem0709@gmail.com>
2023-10-27 16:40:04 +0300

    feat(web): get videos for home page

--- web/src/entities/video.service.ts
+++ web/src/entities/video.service.ts
@@ -1,6 +1,20 @@
 import { apiClient } from "@/entities/api";
 import { Video } from "@/entities/models";
 
+export interface GetVideosRequest {
+  page: number;
+  pageSize: number;
+}
+
+export interface GetVideosResponse {
+  totalCount: number;
+  videos: Video[];
+}
+
+export interface GetSubscriptionsVideosRequest extends GetVideosRequest {
+  accessToken: string;
+}
+
 export interface GetVideosByUserNicknameRequest {
   userNickname: string;
 }
@@ -10,11 +24,41 @@ export interface GetVideosByUserNicknameResponse {
 }
 
 export const videoService = {
-  getVideosByUserNickname: async ({
-    userNickname,
-  }: GetVideosByUserNicknameRequest) => {
+  async getNewVideos(request: GetVideosRequest) {
+    const response = await apiClient.get<GetVideosResponse>(
+      `/videos/?sort_by=new&limit=${request.pageSize}&offset=${
+        (request.page - 1) * request.pageSize
+      }`,
+    );
+    return response.data;
+  },
+
+  async getPopularVideos(request: GetVideosRequest) {
+    const response = await apiClient.get<GetVideosResponse>(
+      `/videos/?sort_by=popular&limit=${request.pageSize}&offset=${
+        (request.page - 1) * request.pageSize
+      }`,
+    );
+    return response.data;
+  },
+
+  async getSubscriptionsVideos(request: GetSubscriptionsVideosRequest) {
+    const response = await apiClient.get<GetVideosResponse>(
+      `/videos/?is_subs=true&limit=${request.pageSize}&offset=${
+        (request.page - 1) * request.pageSize
+      }`,
+      {
+        headers: {
+          Authorization: `Bearer ${request.accessToken}`,
+        },
+      },
+    );
+    return response.data;
+  },
+
+  async getVideosByUserNickname(request: GetVideosByUserNicknameRequest) {
     const response = await apiClient.get<GetVideosByUserNicknameResponse>(
-      `/videos/?user=${userNickname}`,
+      `/videos/?user=${request.userNickname}`,
     );
     return response.data;
   },
--- web/src/pages/home.tsx
+++ web/src/pages/home.tsx
@@ -1,11 +1,22 @@
+import { useState } from "react";
 import { NavItem } from "@/feature/nav-menu";
+import VideoGrid from "@/feature/video-grid";
+import Paginator from "@/shared/paginator";
 import AppBar from "@/widgets/app-bar";
 import Layout from "@/widgets/layout";
 import SideBar from "@/widgets/side-bar";
-import { Typography } from "@mui/joy";
-import { useSearchParams } from "react-router-dom";
+import { useNavigate, useSearchParams } from "react-router-dom";
+import { useQuery } from "@tanstack/react-query";
+import { videoService } from "@/entities/video.service";
+import { Video } from "@/entities/models";
+import { useAppSelector } from "@/feature/store/hooks";
+import { selectAccessToken } from "@/feature/store/auth/auth.selectors";
+import { Box } from "@mui/joy";
 
 export default function Home() {
+  const nav = useNavigate();
+
+  const accessToken = useAppSelector(selectAccessToken);
   const [searchParams] = useSearchParams();
 
   let selectedNavItem: NavItem;
@@ -24,20 +35,85 @@ export default function Home() {
       break;
   }
 
+  const [maxPage, setMaxPage] = useState(0);
+  const pageSize = 16;
+  let page = 1;
+  if (searchParams.has("page") && Number(searchParams.get("page")) > 0) {
+    page = Number(searchParams.get("page"));
+  }
+
+  const [videos, setVideos] = useState<Video[]>([]);
+
+  useQuery({
+    queryKey: ["videos", selectedNavItem, page],
+    queryFn: () => {
+      switch (selectedNavItem) {
+        case NavItem.New:
+          return videoService.getNewVideos({ page, pageSize });
+        case NavItem.Popular:
+          return videoService.getPopularVideos({ page, pageSize });
+        case NavItem.Subscriptions:
+          return videoService.getSubscriptionsVideos({
+            page,
+            pageSize,
+            accessToken: accessToken || "",
+          });
+        default:
+          return videoService.getNewVideos({ page, pageSize });
+      }
+    },
+    onSuccess: (data) => {
+      setMaxPage(Math.ceil(data.totalCount / pageSize));
+      setVideos(data.videos);
+    },
+    onError: (error) => {
+      console.error(error);
+      setVideos([]);
+    },
+    refetchOnMount: true,
+  });
+
   return (
     <Layout>
       <AppBar />
 
       <SideBar selectedNavItem={selectedNavItem} />
 
-      <Typography
-        fontSize={25}
-        fontWeight={"bold"}
-        textAlign={"center"}
-        style={{ marginTop: 20 }}
+      <Box
+        style={{
+          height: "90vh",
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "start",
+        }}
       >
-        {selectedNavItem}
-      </Typography>
+        <Box
+          style={{
+            flex: 24,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            marginTop: 30,
+            marginLeft: 20,
+            marginRight: 20,
+            overflowY: "auto",
+          }}
+        >
+          <VideoGrid videos={videos} />
+        </Box>
+
+        <Box style={{ flex: 1, alignSelf: "center", marginTop: 10 }}>
+          <Paginator
+            page={page}
+            onChangePage={(newPage) => {
+              nav(`/?page=${newPage}&videos=${selectedNavItem.toLowerCase()}`, {
+                replace: true,
+              });
+            }}
+            maxPage={maxPage}
+          />
+        </Box>
+      </Box>
     </Layout>
   );
 }
