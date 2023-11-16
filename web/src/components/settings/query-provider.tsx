2182d4f74ff861e8b61487f70d5d9a1fe2c41166
protomem <protomem0709@gmail.com>
2023-11-16 23:04:56 +0300

    refacor(web): add axios and react-query

---
+++ web/src/components/settings/query-provider.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    },
+  },
+});
+
+interface QueryProviderProps {
+  children: React.ReactNode;
+}
+
+export function QueryProvider({ children }: QueryProviderProps) {
+  return (
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+  );
+}
--- web/src/pages/_app.tsx
+++ web/src/pages/_app.tsx
@@ -1,16 +1,19 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
-import { ThemeProvider } from "@/components/theme-provider";
+import { ThemeProvider } from "@/components/settings/theme-provider";
+import { QueryProvider } from "@/components/settings/query-provider";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider
-      attribute="class"
-      defaultTheme="dark"
-      disableTransitionOnChange
-    >
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <QueryProvider>
+      <ThemeProvider
+        attribute="class"
+        defaultTheme="dark"
+        disableTransitionOnChange
+      >
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </QueryProvider>
   );
 }
