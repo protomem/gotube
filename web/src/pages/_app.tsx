adf09168981119ea884a2162d6a95a33ae4aadb7
protomem <protomem0709@gmail.com>
2023-11-16 22:09:34 +0300

    refacor(web): app router -> pages router

---
+++ web/src/pages/_app.tsx
@@ -0,0 +1,16 @@
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+
+import { ThemeProvider } from "@/components/theme-provider";
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <ThemeProvider
+      attribute="class"
+      defaultTheme="dark"
+      disableTransitionOnChange
+    >
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
+}
