90e3baeaf27ce5d53e9636a2dce172ba729e14b8
protomem <protomem0709@gmail.com>
2023-12-05 17:39:46 +0300

    feat: add hashing password (bcrypt)

---
+++ internal/hashing/hashing.go
@@ -0,0 +1,10 @@
+package hashing
+
+import "errors"
+
+var ErrWrongPassword = errors.New("wrong password")
+
+type Hasher interface {
+	Generate(password string) (string, error)
+	Compare(password string, hash string) error
+}
