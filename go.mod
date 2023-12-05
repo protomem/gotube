1302cd64d464bcae6fc20e78d1a6cd6d38160279
protomem <protomem0709@gmail.com>
2023-12-05 15:17:53 +0300

    feat: add util libs

--- go.mod
+++ go.mod
@@ -1,3 +1,11 @@
 module github.com/protomem/gotube
 
 go 1.21.3
+
+require (
+	github.com/caarlos0/env/v10 v10.0.0 // indirect
+	github.com/google/uuid v1.4.0 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
+	go.uber.org/multierr v1.11.0 // indirect
+	go.uber.org/zap v1.26.0 // indirect
+)
---
+++ pkg/requestid/requestid.go
@@ -0,0 +1,60 @@
+package requestid
+
+import (
+	"context"
+	"net/http"
+
+	"github.com/google/uuid"
+)
+
+const (
+	Header = "X-Request-Id"
+	Key    = "requestId"
+)
+
+func Middleware() func(http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			rid := r.Header.Get(Header)
+			if rid == "" {
+				rid = Generate()
+				r.Header.Set(Header, rid)
+			}
+
+			ctx := Inject(r.Context(), rid)
+			r = r.WithContext(ctx)
+
+			w.Header().Set(Header, rid)
+
+			next.ServeHTTP(w, r)
+		})
+	}
+}
+
+func Empty() string {
+	return uuid.Nil.String()
+}
+
+func Generate() string {
+	rid, err := uuid.NewRandom()
+	if err != nil {
+		return Empty()
+	}
+
+	return rid.String()
+}
+
+type ctxKey struct{}
+
+func Inject(ctx context.Context, rid string) context.Context {
+	return context.WithValue(ctx, ctxKey{}, rid)
+}
+
+func Extract(ctx context.Context) string {
+	rid, ok := ctx.Value(ctxKey{}).(string)
+	if !ok {
+		return Empty()
+	}
+
+	return rid
+}
