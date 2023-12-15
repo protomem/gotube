9be57bd22181589bc82bb5cbdc96a3ee6aec1972
protomem <protomem0709@gmail.com>
2023-12-15 08:43:59 +0300

    refactor: hiden trace for crash request

--- cmd/api/errors.go
+++ cmd/api/errors.go
@@ -4,22 +4,23 @@ import (
 	"fmt"
 	"log/slog"
 	"net/http"
-	"runtime/debug"
 
 	"github.com/protomem/gotube/internal/response"
 	"github.com/protomem/gotube/internal/validator"
 )
 
 func (app *application) reportServerError(r *http.Request, err error) {
+	// TODO: format trace for logging
 	var (
 		message = err.Error()
 		method  = r.Method
 		url     = r.URL.String()
-		trace   = string(debug.Stack())
+		//trace   = string(debug.Stack())
 	)
 
 	requestAttrs := slog.Group("request", "method", method, "url", url)
-	app.logger.Error(message, requestAttrs, "trace", trace)
+	// app.logger.Error(message, requestAttrs, "trace", trace)
+	app.logger.Error(message, requestAttrs)
 }
 
 func (app *application) errorMessage(w http.ResponseWriter, r *http.Request, status int, message string, headers http.Header) {
